4ec92059c6e362ca7e2991b4e9ab7809ca1e7a83
Amir Rajan <ar@amirrajan.net>
2013-07-04 19:12:43 -0500

    bug fix...

--- watcher_dot_net.rb
+++ watcher_dot_net.rb
@@ -806,7 +806,7 @@ class WatcherDotNet
     build_output = @builder.execute
     puts build_output
     
-    if !build_output =~ /another process/ #supress build failures associated with pdb files
+    if !(build_output =~ /another process/) #supress build failures associated with pdb files
       @notifier.execute "build failed", build_output, 'red' if @builder.failed
     end
 
